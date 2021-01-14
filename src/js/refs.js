15a2317f1f8498c70b3090dfee345af78a36d12a
alinaYamkova <alyamkova@yahoo.com>
2021-01-14 15:51:53 +0200

    коміт1

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export default {
+  form: document.querySelector(".form"),
+
+  input: document.getElementById("query"),
+
+  searchBtn: document.querySelector(".searchBtn"),
+  gallery: document.querySelector(".gallery"),
+  // получаем доступ к кнопке, которая создается при отрисовке первого запроса
+  loadMoreBtn: document.querySelector(".loadMore"),
+};
